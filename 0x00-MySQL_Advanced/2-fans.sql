1c0d23f43086d6b50bf7e8c2bcee7f42a3a0dd4a
Dinobi <architectstan01@gmail.com>
2023-08-11 18:24:10 +0100

    Storage Backend

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- A SQL script that ranks country origins of bands
 
-SELECT SUM(fans) AS nb_fans, origin FROM metal_bands
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
