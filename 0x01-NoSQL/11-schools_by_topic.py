1c29699c55287e66f807628e91cf91bd0fdbf5fd
Dinobi <architectstan01@gmail.com>
2023-08-14 20:47:10 +0100

    NOSQL

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import pymongo
 """A python function that returns the list of school having a specific topic"""
 def schools_by_topic(mongo_collection, topic):
     """Lists schools of specific topics"""
