61c736a507914403e6f222fa1edaf97a573cbed5
Dinobi <architectstan01@gmail.com>
2023-08-14 19:09:06 +0100

    NOSQL

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""""""
+""" Changes the topic of a school"""
 def update_topics(mongo_collection, name, topics):
+    """Changes the topic of a school"""
     doc = mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
     return doc
