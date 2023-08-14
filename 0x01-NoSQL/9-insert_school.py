2644d135b2d2f3ee120e9d79ddf0bfe0ae32302a
Dinobi <architectstan01@gmail.com>
2023-08-14 18:34:01 +0100

    NOSQL

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""A python function that inserts a new document in a collections based kwargs"""
+def insert_school(mongo_collection, **kwargs):
+    """Inserts the new document in a collection"""
+    doc_id =  mongo_collection.insert_one(kwargs)
+    return doc_id
